17efe170d0a03a65256ecadd3afbf2f3eb92404a
asyiffaurrohman <akungithub934@gmail.com>
2026-06-11 16:10:18 +0700

    menambahkan penanganan untuk opsi menu yang tidak valid dalam fungsi main

--- Serachthree.cpp
+++ Serachthree.cpp
@@ -179,4 +179,12 @@ int main()
             return 0;
 
         default:
+      {
+            cout << "Invalid option" << endl;
+            break;
+        }
+        }
         
+    return 0;
+}
+}                    
